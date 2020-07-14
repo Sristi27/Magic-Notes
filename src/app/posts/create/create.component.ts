0d5c4519b489f55fece4395cef925af95bbcc0c4
Sristi27 <sristi2705@gmail.com>
2020-07-14 18:19:57 +0530

    Image Added

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Post } from '../model/post.model';
 import { PostsService } from '../services/posts.service'
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
@@ -14,13 +14,24 @@ export class CreateComponent implements OnInit {
   enteredTitle: string = "";
   enteredContent: string = "";
   post: Post;
+  imagePreview:string;
   postId:string;
+  form:FormGroup;
   mode = 'create';
   constructor(public postService: PostsService,
     private router: Router,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+   this.form=new FormGroup({
+     'title':new FormControl(null,{validators:[Validators.required,Validators.minLength(3)]}),
+     'content':new FormControl(null,{validators:[Validators.required]}),
+     'image':new FormControl(null,{validators:[Validators.required]})
+   })
+
+
+
     this.route.paramMap.subscribe(
       (paramMap: ParamMap) => {
         if (paramMap.has('postId')) {
@@ -29,9 +40,14 @@ export class CreateComponent implements OnInit {
           this.postService.getPostById(this.postId).subscribe(
             postData=>{
 
-              this.post={id:postData._id,title:postData.title,content:postData.content}
+              this.post={id:postData._id,title:postData.title,content:postData.content};
+              this.form.setValue({
+                'title':this.post.title,
+                'content':this.post.content,
+              })
             }
           );
+          
         }
         else {
           this.mode = 'create';
@@ -44,16 +60,30 @@ export class CreateComponent implements OnInit {
 
 
 
-  onAddPost(f: NgForm) {
-    console.log(f.value)
+  onAddPost() {
+    
     if(this.mode=='create'){
-      this.postService.addPost(f.value.title, f.value.content);
+      this.postService.addPost(this.form.value.title,this.form.value.content);
     }
     else{
-      this.postService.updatePost(this.postId,f.value.title,f.value.content);
+      this.postService.updatePost(this.postId,this.form.value.title,this.form.value.content);
     }
     
-    f.resetForm();
+    this.form.reset();
     this.router.navigate(['/']);
+  }
+
+
+  onImagePicked(event:Event){
+
+    const file= (event.target as HTMLInputElement).files[0];  //.files gives array of files
+    this.form.patchValue({image:file});  //targets a single control
+    this.form.get('image').updateValueAndValidity()  //tells angular that changes have been made
+    const reader=new FileReader;
+    reader.onload = ()=>{
+      this.imagePreview=reader.result as string;
+    }
+    reader.readAsDataURL(file);
+
   }
 }
